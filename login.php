1089cf78389600eb30009adce6a43f763a83465f
adjimuhamadzidan <adjimuhamadzidan@gmail.com>
2023-02-06 22:39:25 +0700

    update file asset
    update file asset

--- login.php
+++ login.php
@@ -61,7 +61,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Satria Bangsa | Log in</title>
-
+  <link rel="icon" type="image/x-icon" href="logo/pencaksilat.ico">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
